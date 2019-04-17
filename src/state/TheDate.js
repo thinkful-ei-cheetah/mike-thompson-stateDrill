3dcef7ddf3c585041a270f5df2eacad679391948
Mike Jung <kpnba90@gmail.com>
2019-04-17 10:03:39 -0400

    started playground

---
+++ src/state/TheDate.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TheDate extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return <div />
+  }
+}
+
+export default TheDate
