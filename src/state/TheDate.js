cdc1aee0fe2946f91c9775f6f5dd4a81add4010d
Mike Jung <kpnba90@gmail.com>
2019-04-17 12:12:32 -0400

    starting state drill

--- src/state/TheDate.js
+++ src/state/TheDate.js
@@ -3,10 +3,28 @@ import React from 'react';
 class TheDate extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = { datetime: new Date() };
+    console.log('constructor')
   }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+    this.interval = setInterval(() => {
+      console.log('setInterval')
+      this.setState({
+        datetime: new Date()
+      })
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
   render() {
-    return <div />
+    return (
+      <div>{this.state.datetime.toLocaleString()}</div>
+    )
   }
 }
 
